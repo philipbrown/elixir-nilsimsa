c74028616ac7fa75d34c7ba4ac94d2834f675c54
Philip Brown <phil@ipbrown.com>
2021-07-12 22:36:47 +0100

    First commit

---
+++ lib/nilsimsa.ex
@@ -0,0 +1,18 @@
+defmodule Nilsimsa do
+  @moduledoc """
+  Documentation for `Nilsimsa`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nilsimsa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nilsimsa_test.exs
@@ -0,0 +1,8 @@
+defmodule NilsimsaTest do
+  use ExUnit.Case
+  doctest Nilsimsa
+
+  test "greets the world" do
+    assert Nilsimsa.hello() == :world
+  end
+end
