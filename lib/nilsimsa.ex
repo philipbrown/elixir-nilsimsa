80d7f9d42ac53840267e58aae6841615c0f408c4
Philip Brown <phil@ipbrown.com>
2021-07-14 21:51:52 +0100

    Finish off Nilsimsa implementation

--- lib/nilsimsa.ex
+++ lib/nilsimsa.ex
@@ -3,32 +3,67 @@ defmodule Nilsimsa do
   Nilsimsa
   """
 
+  use Bitwise, skip_operators: true
+
+  import Enum, only: [at: 2]
+
+  @tran3 :nilsimsa
+         |> Application.app_dir("priv/tran3")
+         |> File.read!()
+         |> String.split("\n")
+         |> Enum.map(&String.to_integer(&1))
+
+  @popc :nilsimsa
+        |> Application.app_dir("priv/popc")
+        |> File.read!()
+        |> String.split("\n")
+        |> Enum.map(&String.to_integer(&1))
+
   @type t :: %__MODULE__{
           acc: list(integer()),
           count: integer(),
           digest: list(integer()) | nil,
+          threshold: float(),
           window: list(integer())
         }
 
   defstruct acc: for(_ <- 0..255, do: 0),
             count: 0,
             digest: nil,
+            threshold: 0.0,
             window: for(_ <- 0..3, do: -1)
 
   @doc """
   """
   @spec compare(t, t) :: integer
-  def compare(_a, _b) do
-    # TODO: make it work
-    0
+  def compare(%{digest: a}, %{digest: b}) when is_list(a) and is_list(b) do
+    Enum.reduce(0..31, 128, fn i, acc ->
+      acc - at(@popc, band(255, bxor(at(a, i), at(b, i))))
+    end)
+  end
+
+  def compare(a, b) do
+    compare(digest(a), digest(b))
   end
 
   @doc """
   """
   @spec digest(t) :: t
   def digest(nilsimsa) do
-    # TODO: make it work
-    nilsimsa
+    nilsimsa = %{nilsimsa | threshold: trigrams(nilsimsa) / 256}
+
+    digest =
+      Enum.reduce(0..255, for(_ <- 0..31, do: 0), fn i, digest ->
+        if at(nilsimsa.acc, i) > nilsimsa.threshold do
+          List.update_at(digest, bsr(i, 3), fn n ->
+            n + bsl(1, band(i, 7))
+          end)
+        else
+          digest
+        end
+      end)
+
+    %{nilsimsa | digest: Enum.reverse(digest)}
   end
 
   @doc """
@@ -38,13 +73,86 @@ defmodule Nilsimsa do
     process(binary, %__MODULE__{})
   end
 
+  @doc """
+  """
   @spec process(String.t(), t) :: t
-  def process(<<_head::utf8, _tail::binary>>, nilsimsa) do
-    # TODO: make it work
-    nilsimsa
+  def process(<<c::utf8, rest::binary>>, %{acc: acc, window: win} = nilsimsa) do
+    acc =
+      if at(win, 1) > -1 do
+        List.update_at(acc, tran3(c, at(win, 0), at(win, 1), 0), &(&1 + 1))
+      else
+        acc
+      end
+
+    acc =
+      if at(win, 2) > -1 do
+        acc
+        |> List.update_at(tran3(c, at(win, 0), at(win, 2), 1), &(&1 + 1))
+        |> List.update_at(tran3(c, at(win, 1), at(win, 2), 2), &(&1 + 1))
+      else
+        acc
+      end
+
+    acc =
+      if at(win, 3) > -1 do
+        acc
+        |> List.update_at(tran3(c, at(win, 0), at(win, 3), 3), &(&1 + 1))
+        |> List.update_at(tran3(c, at(win, 1), at(win, 3), 4), &(&1 + 1))
+        |> List.update_at(tran3(c, at(win, 2), at(win, 3), 5), &(&1 + 1))
+        |> List.update_at(tran3(at(win, 3), at(win, 0), c, 6), &(&1 + 1))
+        |> List.update_at(tran3(at(win, 3), at(win, 2), c, 7), &(&1 + 1))
+      else
+        acc
+      end
+
+    nilsimsa =
+      nilsimsa
+      |> Map.put(:acc, acc)
+      |> Map.update!(:count, &(&1 + 1))
+      |> Map.update!(:window, &List.insert_at(List.delete_at(&1, 3), 0, c))
+
+    process(rest, nilsimsa)
   end
 
   def process(<<>>, nilsimsa) do
     nilsimsa
+  end
+
+  ###########
+  # Private #
+  ###########
+
+  defp tran3(a, b, c, n) do
+    @tran3
+    |> at(a + n)
+    |> band(255)
+    |> bxor(at(@tran3, b) * (n + n + 1))
+    |> Kernel.+(at(@tran3, bxor(c, at(@tran3, n))))
+    |> band(255)
+  end
+
+  defp trigrams(%{count: 3}), do: 1
+  defp trigrams(%{count: 4}), do: 4
+  defp trigrams(%{count: count}) when count > 4, do: 8 * count - 28
+  defp trigrams(_), do: 0
+end
+
+defimpl String.Chars, for: Nilsimsa do
+  @moduledoc """
+  String.Chars protocol for Nilsimsa
+  """
+
+  import Kernel, except: [to_string: 1]
+
+  def to_string(%{digest: nil} = nilsimsa) do
+    nilsimsa
+    |> Nilsimsa.digest()
+    |> to_string()
+  end
+
+  def to_string(nilsimsa) do
+    nilsimsa.digest
+    |> :binary.list_to_bin()
+    |> Base.encode16(case: :lower)
   end
 end
--- test/nilsimsa_test.exs
+++ test/nilsimsa_test.exs
@@ -1,10 +1,34 @@
 defmodule NilsimsaTest do
   use ExUnit.Case
+
   doctest Nilsimsa
 
-  test "compare"
+  # Examples take from:
+  # https://github.com/rholder/nilsimsa/blob/110b236de3505e4ee64d485f7499d4ce1cf34cc6/src/test/java/com/github/rholder/nilsimsa/NilsimsaHashTest.java
 
-  test "digest"
+  @a k: "abcdefgh",
+     v: "14c8118000000000030800000004042004189020001308014088003280000078"
 
-  test "process"
+  @b k: "This is a much more ridiculous test because of 21347597.",
+     v: "5d9c6a6b22384bcd524a8d414d82237777433fc1a07a02c3e06985d96ecdf8fb"
+
+  @c k: "The rain in Spain falls mostly in the plains.",
+     v: "039020eb1050188be400091130981860648e39f5b1246d8c3c3c7623801186ac"
+
+  @d k: "The rain in Spain falls mainly in the plains.",
+     v: "23b000e908501883c408019410d83a60c48f1977a3246ccc3cbc7213c81104bc"
+
+  test "compare" do
+    a = Nilsimsa.process(@a[:k])
+    b = Nilsimsa.process(@b[:k])
+
+    assert Nilsimsa.compare(a, b) == 4
+    assert Nilsimsa.compare(b, a) == 4
+  end
+
+  for n <- [@a, @b, @c, @d] do
+    test "process: #{n[:k]}" do
+      assert to_string(Nilsimsa.process(unquote(n[:k]))) == unquote(n[:v])
+    end
+  end
 end
