ad8b361c18f7d968e97f1914d0bf3a6d5649fc31
Philip Brown <phil@ipbrown.com>
2021-07-15 22:03:18 +0100

    Add license and package details

--- mix.exs
+++ mix.exs
@@ -4,8 +4,10 @@ defmodule Nilsimsa.MixProject do
   def project do
     [
       app: :nilsimsa,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.11",
+      package: package(),
+      description: description(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       preferred_cli_env: [
@@ -17,20 +19,29 @@ defmodule Nilsimsa.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test, runtime: false}
+    ]
+  end
+
+  defp description() do
+    "Nilsimsa locality-sensitive hashing algorithm in Elixir."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/philipbrown/elixir-nilsimsa"}
     ]
   end
 end
